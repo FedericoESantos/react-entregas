7b33b3bc7e4bf372ccda356144813e5862fab403
FedericoESantos <boomarts@yahoo.com>
2024-01-13 17:20:04 -0300

    PROYECTO DE REACT TERMINADO - FEDERICO EDUARDO SANTOS

--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -3,11 +3,17 @@ import React from 'react'
 const Contacto = () => {
     return (
         <div className='contacto'>
-        <img src="../../../public/images/logo.png" alt="" />
+        <img src="../../../public/images/logo.png" alt="logo" className='logo'/>
             <h1> Contáctenos por cualquier duda o sugerencia</h1>
+            <img src="../../../public/images/whatsapp.png" alt="whats" />
             <h2>Comunicarse al: <p> (+54) 221 301 1421</p> </h2>
-            <div className='redes'>
-                
+            <div>
+                <div className="redes">
+                    <img src="../../../public/images/facebook.png" alt="logo-face" />
+                    <p>: Pizza-Flash</p>
+                    <img src="../../../public/images/instagram.png" alt="logo-insta" />
+                    <p>: Pizza-Flash</p>
+                </div>
             </div>
             <figure>
                 <img src="../../../public/images/flash-cont.png" alt="flash" />
